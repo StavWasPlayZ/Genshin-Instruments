98940b8e8d2bfad62e4bda23158dd40cdf03bfd9
StavWasPlayZ <stavsolomon101@gmail.com>
2023-04-29 23:31:55 +0300

    Bumped version to v2.3

--- src/main/java/com/cstav/genshinstrument/event/InstrumentPlayedEvent.java
+++ src/main/java/com/cstav/genshinstrument/event/InstrumentPlayedEvent.java
@@ -5,6 +5,10 @@ import com.cstav.genshinstrument.sound.NoteSound;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.eventbus.api.Event;
 
+/**
+ * An event that fires when an instrument produces a sound.
+ * This event is fired on the Forge event bus.
+ */
 public class InstrumentPlayedEvent extends Event {
     
     public final NoteSound sound;
