cb1cc74d8cb1fb5f9b6f21db6eea617c057960bc
StavWasPlayZ <stavsolomon101@gmail.com>
2023-05-26 23:00:00 +0300

    More multiloader implementation

---
+++ Fabric/src/main/java/com/cstav/genshinstrument/Main.java
@@ -0,0 +1,14 @@
+package com.cstav.genshinstrument;
+
+import com.mojang.logging.LogUtils;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        LogUtils.getLogger().info("YASSS");
+    }
+    
+}
