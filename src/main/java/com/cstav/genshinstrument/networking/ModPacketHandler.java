9f23163c8f3bd2e69a6d7baace7671b4532f6faf
StavWasPlayZ <stavsolomon101@gmail.com>
2023-08-02 17:54:32 +0300

    Fix invalid handling of packets

--- src/main/java/com/cstav/genshinstrument/networking/ModPacketHandler.java
+++ src/main/java/com/cstav/genshinstrument/networking/ModPacketHandler.java
@@ -2,8 +2,6 @@ package com.cstav.genshinstrument.networking;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-
 import com.cstav.genshinstrument.GInstrumentMod;
 import com.cstav.genshinstrument.networking.buttonidentifier.DefaultNoteButtonIdentifier;
 import com.cstav.genshinstrument.networking.buttonidentifier.DrumNoteIdentifier;
@@ -15,29 +13,28 @@ import com.cstav.genshinstrument.networking.packet.instrument.NotifyInstrumentCl
 import com.cstav.genshinstrument.networking.packet.instrument.NotifyInstrumentOpenPacket;
 import com.cstav.genshinstrument.networking.packet.instrument.OpenInstrumentPacket;
 import com.cstav.genshinstrument.networking.packet.instrument.PlayNotePacket;
-import com.mojang.logging.LogUtils;
+import com.cstav.genshinstrument.util.ServerUtil;
 
-import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.network.NetworkDirection;
 import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.PacketDistributor;
 import net.minecraftforge.network.simple.SimpleChannel;
 
 @EventBusSubscriber(modid = GInstrumentMod.MODID, bus = Bus.MOD)
 public class ModPacketHandler {
-    private static final Logger LOGGER = LogUtils.getLogger();
-
     @SuppressWarnings("unchecked")
-    private static final List<Class<IModPacket>> ACCEPTABLE_PACKETS = List.of(new Class[] {
+    public static final List<Class<IModPacket>> ACCEPTABLE_PACKETS = List.of(new Class[] {
         InstrumentPacket.class, PlayNotePacket.class, OpenInstrumentPacket.class, CloseInstrumentPacket.class,
         NotifyInstrumentOpenPacket.class, NotifyInstrumentClosedPacket.class
     });
+
+    private static int id = 0;
+    public static void registerPackets() {
+        ServerUtil.registerModPackets(INSTANCE, ACCEPTABLE_PACKETS, () -> id++);
+    }
 
 
     @SuppressWarnings("unchecked")
@@ -48,7 +45,6 @@ public class ModPacketHandler {
 
 
     private static final String PROTOCOL_VERSION = "4.3";
-    private static int id;
 
     public static final SimpleChannel INSTANCE = NetworkRegistry.newSimpleChannel(
         new ResourceLocation(GInstrumentMod.MODID, "main"),
@@ -64,36 +60,4 @@ public class ModPacketHandler {
     public static <T> void sendToClient(final T packet, final ServerPlayer player) {
         INSTANCE.send(PacketDistributor.PLAYER.with(() -> player), packet);
     }
-
-    @SubscribeEvent
-    public static void registerPackets(final FMLCommonSetupEvent event) {
-        event.enqueueWork(() -> registerModPackets(INSTANCE, ACCEPTABLE_PACKETS));
-    }
-
-
-    public static void registerModPackets(final SimpleChannel sc, final List<Class<IModPacket>> acceptablePackets) {
-        for (final Class<IModPacket> packetType : acceptablePackets)
-            try {
-                
-                sc.messageBuilder(packetType, id++, (NetworkDirection)packetType.getField("NETWORK_DIRECTION").get(null))
-                    .decoder((buf) -> {
-                        try {
-                            return packetType.getDeclaredConstructor(FriendlyByteBuf.class).newInstance(buf);
-                        } catch (Exception e) {
-                            LOGGER.error("Error constructing packet of type "+packetType.getName(), e);
-                            return null;
-                        }
-                    })
-                    .encoder(IModPacket::toBytes)
-                    .consumerMainThread(IModPacket::handle)
-                .add();
-
-            } catch (Exception e) {
-                LOGGER.error(
-                    "Error registring packet of type "+packetType.getName()
-                        +". Make sure to have a NETWORK_DIRECTION static field of type NetworkDirection."
-                , e);
-            }
-    }
-
 }
