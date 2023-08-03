5925f7dc4985aa5f317607e9809d1833bd9cf854
StavWasPlayZ <stavsolomon101@gmail.com>
2023-08-04 01:04:39 +0300

    Fix creative mode tab translation key

--- src/main/java/com/cstav/genshinstrument/ModCreativeModeTabs.java
+++ src/main/java/com/cstav/genshinstrument/ModCreativeModeTabs.java
@@ -4,13 +4,10 @@ import com.cstav.genshinstrument.item.ModItems;
 
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 
-@EventBusSubscriber(modid = GInstrumentMod.MODID, bus = Bus.MOD)
 public class ModCreativeModeTabs {
 
-    public static final CreativeModeTab instrumentsTab = (new CreativeModeTab("genshinstrument.itemGroup.instruments") {
+    public static final CreativeModeTab instrumentsTab = (new CreativeModeTab("genshinstrument.instruments") {
 
         @Override
         public ItemStack makeIcon() {
