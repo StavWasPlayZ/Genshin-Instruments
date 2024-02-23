2678405952f2b41102ddee042edeacd5cecc8398
StavWasPlayZ <stavsolomon101@gmail.com>
2024-02-23 22:12:17 +0200

    Upgrade mod to MC 1.20.4

--- src/main/java/com/cstav/genshinstrument/criteria/InstrumentPlayedTrigger.java
+++ src/main/java/com/cstav/genshinstrument/criteria/InstrumentPlayedTrigger.java
@@ -1,54 +1,52 @@
 package com.cstav.genshinstrument.criteria;
 
-import java.util.Optional;
-
 import com.cstav.genshinstrument.networking.packet.instrument.InstrumentPacket;
-import com.google.gson.JsonObject;
-
-import net.minecraft.advancements.critereon.AbstractCriterionTriggerInstance;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.advancements.critereon.ContextAwarePredicate;
-import net.minecraft.advancements.critereon.DeserializationContext;
+import net.minecraft.advancements.critereon.EntityPredicate;
 import net.minecraft.advancements.critereon.ItemPredicate;
 import net.minecraft.advancements.critereon.SimpleCriterionTrigger;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.ExtraCodecs;
 import net.minecraft.world.item.ItemStack;
+
+import java.util.Optional;
 
 /**
  * <p>The class holding the genshinstrument_instrument_played trigger.</p>
- * 
+ *
  * <p>
  * It is triggered in {@link InstrumentPacket} such that every sound
  * produced by an instrument will trigger this criteria.
  * It will pass the played instrument from within the {@code instrument} JSON item object.
  * </p>
- * 
+ *
  * Internally, used for triggering advancements for the player.
  */
 public class InstrumentPlayedTrigger extends SimpleCriterionTrigger<InstrumentPlayedTrigger.TriggerInstance> {
-    // It doesn't account for namespaces, so will use genshinstrument_ prefix instead
-    public static final String ID = "genshinstrument_instrument_played";
-    
-
-    @Override
-    protected TriggerInstance createInstance(JsonObject pJson, Optional<ContextAwarePredicate> player,
-            DeserializationContext pContext) {
-        return new TriggerInstance(player, ItemPredicate.fromJson(pJson.get("instrument")));
-    }
 
     public void trigger(final ServerPlayer player, final ItemStack instrument) {
-        trigger(player, (triggerInstance) ->
+        super.trigger(player, (triggerInstance) ->
             triggerInstance.matches(instrument)
         );
     }
 
+    @Override
+    public Codec<TriggerInstance> codec() {
+        return TriggerInstance.CODEC;
+    }
 
-    public static class TriggerInstance extends AbstractCriterionTriggerInstance {
-        private final Optional<ItemPredicate> item;
 
-        public TriggerInstance(Optional<ContextAwarePredicate> pPlayer, Optional<ItemPredicate> item) {
-            super(pPlayer);
-            this.item = item;
-        }
+    public record TriggerInstance(Optional<ContextAwarePredicate> player, Optional<ItemPredicate> item) implements SimpleCriterionTrigger.SimpleInstance {
+        public static final Codec<TriggerInstance> CODEC = RecordCodecBuilder.create((triggerInstance) ->
+            triggerInstance.group(
+                ExtraCodecs.strictOptionalField(EntityPredicate.ADVANCEMENT_CODEC, "player")
+                    .forGetter(TriggerInstance::player),
+                ExtraCodecs.strictOptionalField(ItemPredicate.CODEC, "instrument")
+                    .forGetter(TriggerInstance::item)
+            ).apply(triggerInstance, TriggerInstance::new)
+        );
 
         public boolean matches(final ItemStack instrument) {
             return item.isEmpty() || item.get().matches(instrument);
--- src/main/java/com/cstav/genshinstrument/criteria/ModCriteria.java
+++ src/main/java/com/cstav/genshinstrument/criteria/ModCriteria.java
@@ -1,11 +1,7 @@
 package com.cstav.genshinstrument.criteria;
 
-import com.cstav.genshinstrument.mixins.required.CriterionRegisterInvoker;
-
 import com.cstav.genshinstrument.GInstrumentMod;
 import com.cstav.genshinstrument.event.InstrumentPlayedEvent;
-
-import net.minecraft.advancements.CriterionTrigger;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -13,11 +9,13 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import static net.minecraft.advancements.CriteriaTriggers.register;
+
 //NOTE: There to make it load on setup too
 @EventBusSubscriber(bus = Bus.FORGE, modid = GInstrumentMod.MODID)
 public class ModCriteria {
-
-    public static final InstrumentPlayedTrigger INSTRUMENT_PLAYED_TRIGGER = register(InstrumentPlayedTrigger.ID, new InstrumentPlayedTrigger());
+    // It doesn't account for namespaces, so will use genshinstrument_ prefix instead
+    public static final InstrumentPlayedTrigger INSTRUMENT_PLAYED_TRIGGER = register("genshinstrument_instrument_played", new InstrumentPlayedTrigger());
 
     @SubscribeEvent
     public static void onInstrumentPlayed(final InstrumentPlayedEvent.ByPlayer event) {
@@ -25,9 +23,4 @@ public class ModCriteria {
             INSTRUMENT_PLAYED_TRIGGER.trigger((ServerPlayer)event.player, new ItemStack(ForgeRegistries.ITEMS.getValue(event.instrumentId)));
     }
 
-
-    private static <T extends CriterionTrigger<?>> T register(final String id, final T criterion) {
-        return CriterionRegisterInvoker.callRegister(id, criterion);
-    }
-    
 }
