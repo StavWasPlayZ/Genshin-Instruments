7db169a84c8cce9fbf9a81d7a4d711319c90a9ca
StavWasPlayZ <stavsolomon101@gmail.com>
2024-04-26 05:57:24 +0300

    Load instruments in client initiator and not event bus "subscription"

--- src/main/java/com/cstav/genshinstrument/client/gui/screen/instrument/floralzither/FloralZitherScreen.java
+++ src/main/java/com/cstav/genshinstrument/client/gui/screen/instrument/floralzither/FloralZitherScreen.java
@@ -6,15 +6,11 @@ import com.cstav.genshinstrument.client.gui.screen.instrument.partial.notegrid.G
 import com.cstav.genshinstrument.client.gui.screen.options.instrument.FloralZitherOptionsScreen;
 import com.cstav.genshinstrument.client.gui.screen.options.instrument.partial.InstrumentOptionsScreen;
 import com.cstav.genshinstrument.sound.NoteSound;
-
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
 @OnlyIn(Dist.CLIENT)
-//NOTE: There just to make it load on mod setup
-@EventBusSubscriber(Dist.CLIENT)
 public class FloralZitherScreen extends GridInstrumentScreen {
     public static final ResourceLocation INSTRUMENT_ID = new ResourceLocation(GInstrumentMod.MODID, "floral_zither");
 
