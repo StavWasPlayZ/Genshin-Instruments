bc6bd06cb3e4c372b06e9011879a505a232bdc37
StavWasPlayZ <stavsolomon101@gmail.com>
2024-05-13 18:49:47 +0300

    Convert heldNoteSound to a record

--- src/main/java/com/cstav/genshinstrument/sound/HeldNoteSound.java
+++ src/main/java/com/cstav/genshinstrument/sound/HeldNoteSound.java
@@ -2,18 +2,7 @@ package com.cstav.genshinstrument.sound;
 
 import java.util.Arrays;
 
-public class HeldNoteSound {
-
-    public final NoteSound attack, hold;
-    public final int holdFadeIn, holdFadeOut, holdDelay;
-
-    public HeldNoteSound(NoteSound attack, NoteSound hold, int holdFadeIn, int holdFadeOut, int holdDelay) {
-        this.attack = attack;
-        this.hold = hold;
-        this.holdFadeIn = holdFadeIn;
-        this.holdFadeOut = holdFadeOut;
-        this.holdDelay = holdDelay;
-    }
+public record HeldNoteSound(NoteSound attack, NoteSound hold, int holdFadeIn, int holdFadeOut, int holdDelay) {
 
     public NoteSound getSound(final Phase phase) {
         return switch (phase) {
@@ -21,6 +10,7 @@ public class HeldNoteSound {
             case ATTACK -> attack;
         };
     }
+
     public static NoteSound[] getSounds(final HeldNoteSound[] sounds, final Phase phase) {
         return Arrays.stream(sounds)
             .map((sound) -> sound.getSound(phase))
