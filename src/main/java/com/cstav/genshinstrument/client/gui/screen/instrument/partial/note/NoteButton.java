30aa4232623abea64e03f275d1a50073735a5d46
StavWasPlayZ <stavsolomon101@gmail.com>
2024-07-21 22:38:58 +0300

    Remove incorrect TODO

--- src/main/java/com/cstav/genshinstrument/client/gui/screen/instrument/partial/note/NoteButton.java
+++ src/main/java/com/cstav/genshinstrument/client/gui/screen/instrument/partial/note/NoteButton.java
@@ -34,7 +34,6 @@ public abstract class NoteButton extends AbstractButton {
 
     protected final Minecraft minecraft = Minecraft.getInstance();
 
-    //TODO this is NOT nullable, re-implement DefaultNoteButtonIdentifier
     /**
      * Returns the UI identifier of this button.
      */
