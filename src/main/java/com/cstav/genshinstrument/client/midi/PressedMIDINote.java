1fab2411ca8c24cf56df858febcd76508ed6ce7b
StavWasPlayZ <stavsolomon101@gmail.com>
2024-07-22 01:29:19 +0300

    Mark PressedMIDINote as Client-only

--- src/main/java/com/cstav/genshinstrument/client/midi/PressedMIDINote.java
+++ src/main/java/com/cstav/genshinstrument/client/midi/PressedMIDINote.java
@@ -2,7 +2,10 @@ package com.cstav.genshinstrument.client.midi;
 
 import com.cstav.genshinstrument.client.gui.screen.instrument.partial.note.NoteButton;
 import com.cstav.genshinstrument.sound.NoteSound;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
+@OnlyIn(Dist.CLIENT)
 public record PressedMIDINote(
     int notePitch,
     NoteButton pressedNote,
