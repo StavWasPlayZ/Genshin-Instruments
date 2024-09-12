60a89e906c3c8df8bbc585f91b9f383c77290900
StavWasPlayZ <stavsolomon101@gmail.com>
2024-09-12 23:25:12 +0300

    Fix build errors

--- src/main/java/com/cstav/genshinstrument/client/gui/screen/instrument/partial/grid/GridInstrumentScreen.java
+++ src/main/java/com/cstav/genshinstrument/client/gui/screen/instrument/partial/grid/GridInstrumentScreen.java
@@ -9,6 +9,7 @@ import com.cstav.genshinstrument.client.gui.screen.options.instrument.GridInstru
 import com.cstav.genshinstrument.client.gui.screen.options.instrument.partial.InstrumentOptionsScreen;
 import com.cstav.genshinstrument.client.keyMaps.InstrumentKeyMappings;
 import com.cstav.genshinstrument.client.midi.InstrumentMidiReceiver;
+import com.cstav.genshinstrument.client.util.ClientUtil;
 import com.cstav.genshinstrument.networking.buttonidentifier.NoteButtonIdentifier;
 import com.cstav.genshinstrument.networking.buttonidentifier.NoteGridButtonIdentifier;
 import com.cstav.genshinstrument.sound.NoteSound;
@@ -210,11 +211,11 @@ public abstract class GridInstrumentScreen extends InstrumentScreen {
             renderStaff(stack, i);
     }
 
-    protected void renderClef(GuiGraphics gui, int index, int x, String clefName) {
-        RenderSystem.enableBlend();
+    protected void renderClef(PoseStack stack, int index, int x, String clefName) {
+        ClientUtil.displaySprite(getInternalResourceFromGlob("background/clef/"+clefName+".png"));
 
-        gui.blit(getInternalResourceFromGlob("background/clef/"+clefName+".png"),
-            x, grid.getY() + NoteGrid.getPaddingVert() + getLayerAddition(index) - 5,
+        blit(stack,
+            x, grid.y + NoteGrid.getPaddingVert() + getLayerAddition(index) - 5,
             0, 0,
 
             CLEF_WIDTH, CLEF_HEIGHT,
@@ -223,13 +224,13 @@ public abstract class GridInstrumentScreen extends InstrumentScreen {
 
         RenderSystem.disableBlend();
     }
-    protected void renderStaff(final GuiGraphics gui, final int index) {
-        RenderSystem.enableBlend();
+    protected void renderStaff(final PoseStack stack, final int index) {
+        ClientUtil.displaySprite(getInternalResourceFromGlob("background/staff.png"));
 
-        gui.blit(getInternalResourceFromGlob("background/staff.png"),
-            grid.getX() + 2, grid.getY() + NoteGrid.getPaddingVert() + getLayerAddition(index),
+        blit(stack,
+            grid.x + 2, grid.y + NoteGrid.getPaddingVert() + getLayerAddition(index),
             0, 0,
-            
+
             grid.getWidth() - 5, getNoteSize(),
             grid.getWidth() - 5, getNoteSize()
         );
