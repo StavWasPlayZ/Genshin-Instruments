bf14bef87c8ca732defdb807642d76e0630adbfc
StavWasPlayZ <stavsolomon101@gmail.com>
2025-04-05 10:46:59 +0300

    Add separate textures for Ukulele top column note buttons

--- src/main/java/com/cstav/genshinstrument/client/gui/screen/instrument/ukelele/UkuleleNoteButtonRenderer.java
+++ src/main/java/com/cstav/genshinstrument/client/gui/screen/instrument/ukelele/UkuleleNoteButtonRenderer.java
@@ -8,20 +8,43 @@ import net.minecraft.resources.ResourceLocation;
 import java.util.function.Supplier;
 
 public class UkuleleNoteButtonRenderer extends NoteButtonRenderer {
+    protected final ResourceLocation
+        topColumnNotePressedLocation,
+        topColumnNoteReleasedLocation,
+        topColumnNoteHoverLocation
+    ;
+
     public UkuleleNoteButtonRenderer(NoteButton noteButton, Supplier<ResourceLocation> labelTextureProvider) {
         super(noteButton, labelTextureProvider);
+
+        topColumnNotePressedLocation = getResourceFromRoot("note/top_pressed.png");
+        topColumnNoteReleasedLocation = getResourceFromRoot("note/top_released.png");
+        topColumnNoteHoverLocation = getResourceFromRoot("note/top_hovered.png");
     }
 
     private NoteGridButton getButton() {
         return (NoteGridButton) noteButton;
     }
 
+
     @Override
     protected ResourceLocation getNoteReleasedLocation() {
+        return getTopColumnOverride(topColumnNoteReleasedLocation, super.getNoteReleasedLocation());
+    }
+    @Override
+    protected ResourceLocation getNotePressedLocation() {
+        return getTopColumnOverride(topColumnNotePressedLocation, super.getNotePressedLocation());
+    }
+    @Override
+    protected ResourceLocation getNoteHoverLocation() {
+        return getTopColumnOverride(topColumnNoteHoverLocation, super.getNoteHoverLocation());
+    }
+
+    private ResourceLocation getTopColumnOverride(final ResourceLocation newLocation, final ResourceLocation superLocation) {
         if (getButton().column == 0) {
-            return notePressedLocation;
+            return newLocation;
         }
 
-        return super.getNoteReleasedLocation();
+        return superLocation;
     }
 }
