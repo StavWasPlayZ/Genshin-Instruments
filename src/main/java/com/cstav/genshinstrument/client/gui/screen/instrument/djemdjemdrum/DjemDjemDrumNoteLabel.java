e405bcf8af020566b5d535354fe26eddd4f2db55
StavWasPlayZ <stavsolomon101@gmail.com>
2025-04-06 12:54:12 +0300

    Add note notation and do re mi to Djem Djem Drum labels

--- src/main/java/com/cstav/genshinstrument/client/gui/screen/instrument/djemdjemdrum/DjemDjemDrumNoteLabel.java
+++ src/main/java/com/cstav/genshinstrument/client/gui/screen/instrument/djemdjemdrum/DjemDjemDrumNoteLabel.java
@@ -4,6 +4,8 @@ import com.cstav.genshinstrument.client.config.ModClientConfigs;
 import com.cstav.genshinstrument.client.gui.screen.instrument.partial.note.NoteButton;
 import com.cstav.genshinstrument.client.gui.screen.instrument.partial.note.label.INoteLabel;
 import com.cstav.genshinstrument.client.gui.screen.instrument.partial.note.label.NoteLabelSupplier;
+import com.cstav.genshinstrument.util.LabelUtil;
+import net.minecraft.network.chat.Component;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
@@ -20,12 +22,12 @@ public enum DjemDjemDrumNoteLabel implements INoteLabel {
 //	DON_KA((note) ->
 //		Component.translatable(dn(note).btnType.getTransKey())
 //	),
-//	NOTE_NAME((note) -> Component.literal(
-//		note.getFormattedNoteName()
-//	)),
-//	DO_RE_MI((note) ->
-//        LabelUtil.toDoReMi(note.getFormattedNoteName())
-//    ),
+	NOTE_NAME((note) -> Component.literal(
+		note.getFormattedNoteName()
+	)),
+	DO_RE_MI((note) ->
+        LabelUtil.toDoReMi(note.getFormattedNoteName())
+    ),
 
     NONE(NoteLabelSupplier.EMPTY);
 
